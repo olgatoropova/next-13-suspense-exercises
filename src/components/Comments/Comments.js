4d9e7ed9525e25b527f6159944ae2e7283555d80
Olga Toropova <fluctuacia@gmail.com>
2023-09-29 13:02:24 -0700

    Loading comments async from main page

---
+++ src/components/Comments/Comments.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { getComments } from '@/helpers/interview-helpers';
+import Comment from '@/components/Comment';
+
+async function Comments() {
+	const comments = await getComments();
+
+	return (
+		<>
+			{comments.map((comment) => (
+				<Comment key={comment.id} comment={comment} />
+			))}
+		</>
+	);
+}
+
+export default Comments;
